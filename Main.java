0b56d65766adb92568257ebf65425ff02bc37e54
lifemailtosumit <40574938+lifemailtosumit@users.noreply.github.com>
2024-04-10 11:27:24 +0530

    login-called

--- Main.java
+++ Main.java
@@ -1,5 +1,6 @@
 class Main{
 	public static void main(String arg[]){
-		Sysytem.out.println("Hello everyone");
+		System.out.println("Hello everyone");
+		System.out.println("Login called ");
 	}
 }
